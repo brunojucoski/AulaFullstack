2aba9f253fafe1b7e5dfdce1b9603bbf8ebe6959
Bruno Albuquerque Vaz Jucoski <141188686+brunojucoski@users.noreply.github.com>
2024-08-30 21:38:55 -0300

    Add files via upload

---
+++ exemplo-security-api2/src/main/java/br/com/senac2/exemplo_security_api2/useCase/domains/UsuarioRequestDom.java
@@ -0,0 +1,22 @@
+package br.com.senac2.exemplo_security_api2.useCase.domains;
+
+public class UsuarioRequestDom {
+    private String login;
+    private String senha;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
